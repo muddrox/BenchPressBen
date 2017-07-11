b5795c8fde495275f5cb87a1bd8697d7e44d2e9e
willEitan <wil15119@byui.edu>
2017-07-11 01:06:44 -0600

    I created an x and y variable as well as jitter function to make acid display. Lose messages are now selected randomly from the enum!

--- core/src/messages/Loser.java
+++ core/src/messages/Loser.java
@@ -2,6 +2,7 @@ package messages;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import static com.badlogic.gdx.math.MathUtils.random;
 
 
 /**
@@ -9,21 +10,31 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
  */
 
 public class Loser{
-
+    /**
+     * private variables of loser class are text, font, x, and y
+     */
     private String text;
     private BitmapFont textFont;
+    private int x;
+    private int y;
+
 
     /**
      * Loser constructor initializes private variables:
-     * text font size and type
+     * text font size and type. Constructor also set lose message
+     * randomly from the enum
      *
      * @author Ethan Williams
      */
     public Loser() {
+
         textFont = new BitmapFont(Gdx.files.internal("arial_large.fnt"));
         textFont.getData().setScale(2f, 2f);
-        textFont.setColor(0f, 9f, 4f, 0f);
-        text = Messages.ONE.toString();
+        textFont.setColor(0f, 1f, 1f, 1f);
+        Messages youLost = Messages.randomMess();
+        text = youLost.toString();
+        setX(135);
+        setY(665);
     }
 
     /**
@@ -36,5 +47,41 @@ public class Loser{
 
     public BitmapFont getTextFont() {
         return textFont;
+    }
+
+    /**
+     * basic getters and setters for x and y variables
+     *
+     * @return int & void
+     */
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    /**
+     * getters for jitx and jity are for creating a jitter display
+     *
+     * @return int
+     */
+    public int getJitX(){
+        int jitX = random(-12, 13);
+        return (getX() + jitX);
+    }
+
+    public int getJitY(){
+        int jitY = random(-12, 13);
+        return (getY() + jitY);
     }
 }
